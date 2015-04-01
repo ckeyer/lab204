899deae4c55bf3a5c82e328155a04a2bcea0ee7d
sllt <shenlongml@gmail.com>
2015-04-01 11:28:18 +0800

    update user model op

--- models/user_topic_relation.go
+++ models/user_topic_relation.go
@@ -1,8 +1,15 @@
 package models
 
+import (
+	"github.com/astaxie/beego/orm"
+	"time"
+)
+
 type UserTopicRelation struct {
 	Id         int64
 	UserId     int64
 	TopicId    int64
 	ActionType string
+	Created    time.Time
+	Updated    time.Time
 }
